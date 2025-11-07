95014b904e127ebb82efc20b2f36d310ea1875e0
Kaelo <ksquarekareem@gmail.com>
2025-11-07 10:54:08 +0100

    Print to console

---
+++ index2.js
@@ -0,0 +1,5 @@
+/**
+ * Create a script to print the following: I love jabascript, This is my heritage, I am from nigeria.
+ * Print on three lines
+ */
+console.log("I love javscript\nThis is my Heritage\nI am from nigeria")
