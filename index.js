83e62ad8160f5102ffa1d6b9454120c8796cb60f
Kaelo <ksquarekareem@gmail.com>
2025-11-07 10:40:47 +0100

    Print to console

---
+++ index.js
@@ -0,0 +1,9 @@
+/**
+ * (1) Create a script to print javascript is fun to the console
+ * You're not allowed to use var
+ * You're only to write your script in one line
+ *
+ */
+
+// Script to print a text
+console.log("Javascript is fun")
