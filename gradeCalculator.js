1d811ffa1ead457b859a030297479ffb14149343
Kaelo <ksquarekareem@gmail.com>
2025-11-07 12:30:26 +0100

    Print to console

--- gradeCalculator.js
+++ gradeCalculator.js
@@ -8,16 +8,35 @@
  */
 
 function calculateGrade(score) {
-    if (score >= 90 && score <= 100 ) {
+    // if (score >= 90 && score <= 100 ) {
+    //     return "A" 
+    // } 
+    // else if (score >= 80 && score <= 89) {
+    //         return "B"
+    //     }
+    // else if (score >= 70 && score <= 79) {
+    //         return "C"
+    //     }
+    // else if (score >= 60 && score <= 69) {
+    //         return "D"
+    //     }
+    // else if (score < 60) {
+    //         return "F"    
+    //     }
+    // else  {
+    //     return "score is invalid"
+    // }  
+
+       if (score >= 90 && score <= 100 ) {
         return "A" 
     } 
-    else if (score >= 80 && score <= 89) {
+    else if (score >= 80) {
             return "B"
         }
-    else if (score >= 70 && score <= 79) {
+    else if (score >= 70) {
             return "C"
         }
-    else if (score >= 60 && score <= 69) {
+    else if (score >= 60) {
             return "D"
         }
     else if (score < 60) {
@@ -26,6 +45,7 @@ function calculateGrade(score) {
     else  {
         return "score is invalid"
     }  
+
 
 } 
 
