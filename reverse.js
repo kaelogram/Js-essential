6f69dc0592608311a29cc42092eafbf802f1ae7b
Kaelo <ksquarekareem@gmail.com>
2025-11-12 09:59:36 +0100

    reverse.js

--- reverse.js
+++ reverse.js
@@ -2,14 +2,19 @@
  * Create a function that reverses a string without using built-in reverse() method
  */
 function reverseString(str) {
-    let reversed = ""
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed += str[i]
-    }
-
-    return reversed;
-
+  return str.split("").reverse().join("");
 }
+
+// console.log(reverseString("hello"));
+
+//     let reversed = ""
+//     for (let i = str.length - 1; i >= 0; i--) {
+//         reversed += str[i]
+//     }
+
+//     return reversed;
+
+// }
 
 // Test cases
 console.log (reverseString ("hello"));
